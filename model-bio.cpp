b4392ab1c9dc2954c2202e6da47098ebe4b0c1ef
vitor <vitors.hirata@gmail..com>
2017-11-24 02:44:33 -0200

    Improve comments of the model-bio code.

--- model-bio.cpp
+++ model-bio.cpp
@@ -13,12 +13,12 @@ const int n = 3;// number of resources
 const int k = 100; // number of species
 const int nK = 7; // number of bytes to represent species
 const float u = 0.1; // death probability
-const float m = 0.0; //0.01; // mutation probability
+const float m = 0.0; // mutation probability
 const int T = 100000; // maximum time
 const int tic = 1000; // tic interval in time
-const int nRun = 10;  // numero de rodadas
+const int nRun = 10;  // number of runs to average
 class patch;
-double K[k*n];
+double K[k*n]; // vector containing the half saturation constants
 
 static std::random_device rd;
 static std::mt19937_64 rand64(rd());
@@ -28,31 +28,37 @@ static std::uniform_int_distribution<long> uniIntne(0,4 - 1);
 static std::uniform_real_distribution<double> uniFLOAT(0.0,1.0);
 static std::normal_distribution<double> gauss(1.0,0.1);
 
+
+// Each patch equivales to one produceble site, and contain up to one plantation an the resources present in the soil.
 class patch{
   std::vector<float> resource; // each number is the amount of the i'th resource
   public:
-    boost::dynamic_bitset<> specie;
+    boost::dynamic_bitset<> specie; // binary array
     void kill(void);
     double fitness(void);
     patch(void);
     ~patch();
 };
 
+// Constructor, initialize resouce with n-sized vector and uniform distribution [0,1], and specie as an int [0,k], convert to an array of binary (size nK bytes).
 patch::patch(void){
   for(int i=0;i<n;i++)
     resource.push_back(uniFLOAT(rand64));
   specie = boost::dynamic_bitset<>(nK,uniIntk(rand64));
 }
 
+// Destructor, free the memory of resource and specie.
 patch::~patch(void){
   std::vector<float>().swap(resource);
   specie = boost::dynamic_bitset<>(0);
 }
 
+// Uses XOR to make specie an empty array of binaries. No output.
 void patch::kill(void){
   specie ^= specie;
 }
 
+// Return the fitness of the population living in the site. Computed using the Monod equation.s
 double patch::fitness(void){
   double min = resource[0]/(K[(specie.to_ulong()-1)*n]+resource[0]);
   for (int i=1;i<n;i++){
@@ -68,10 +74,12 @@ void iterate(patch grid[]);
 int countSpecie(patch grid[]);
 void Run(void);
 
+// Receives the grid and iterate one time, passing over all the sites of the grid.
 void iterate(patch grid[]){
   int x, y, x_Neigh, y_Neigh;
   std::vector<int> x_list(L), y_list(L);
 
+  // Initialize the list over which the program will pass updating the program.
   for(int i=0;i<L;i++)
     x_list[i]=y_list[i]=i;
   std::random_shuffle(x_list.begin(),x_list.end());
@@ -83,12 +91,12 @@ void iterate(patch grid[]){
       x = x_list[i];
       y = y_list[i];
       if(grid[x*L+y].specie.any()){
-        if(uniFLOAT(rand64) < u)
+        if(uniFLOAT(rand64) < u) // death probability
           grid[x*L+y].kill();
         else
           if(haveNeighbor(grid, x, y, &x_Neigh,&y_Neigh) && uniFLOAT(rand64) < grid[x*L+y].fitness()){
             grid[x_Neigh*L+y_Neigh].specie = grid[x*L+y].specie;
-            if(uniFLOAT(rand64) < m){
+            if(uniFLOAT(rand64) < m){ // mutation probability
               int temp = uniIntkb(rand64);
               grid[x_Neigh*L+y_Neigh].specie[temp] = !grid[x_Neigh*L+y_Neigh].specie[temp];
             }
@@ -97,6 +105,7 @@ void iterate(patch grid[]){
     }
 }
 
+// Receives the grid, one site (x,y), and a pair of pointers (*x_N, *y_N) where the result will be put. Return false if there aren't empty neighboor and true if there is at least one empy neighboor. Sampling one random site if there are more than one.
 bool haveNeighbor(patch grid[], int x, int y, int* x_N, int* y_N){
   int xP = x+1, xM = x-1, yP = y+1, yM = y-1;
   std::vector<int> PossibleX(4), PossibleY(4);
@@ -144,6 +153,7 @@ bool haveNeighbor(patch grid[], int x, int y, int* x_N, int* y_N){
   }
 }
 
+// Cont the number of observed species at the actual time, on the grid.
 int countSpecie(patch grid[]){
   int nDifferentS=0;
   std::vector<bool> alreadyExist(k+1,false);
@@ -162,6 +172,8 @@ int countSpecie(patch grid[]){
     return nDifferentS;
 }
 
+
+// Run the complete model, making nRun rows and putting the result in one txt.
 void Run(void){
   std::vector<int> result(T/tic,0);
   fstream arquivo;
