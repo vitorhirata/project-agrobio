b35187359a542290a16a02f210bbb492564832ee
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2018-05-22 17:22:33 -0300

    [WIP] Rebuild punctuation system
    - Also remove old codes

--- social_model/propriety.hpp
+++ social_model/propriety.hpp
@@ -1,21 +1,15 @@
 class propriety{
 private:
-  float probDistSigma(float x);
   std::vector<int> speciesChoosen;
   std::vector<int> availableSpecie;
   std::vector<float> speciesPunctuation;
 public:
   std::vector<int> proprietyConnection;
-
   int LimN;
   int LimS;
   int LimL;
   int LimO;
-  float probInovation;
   float personalPref;
-  float weight[4];
-  int maxSpecie;
-  float maxSpeciePunc;
   void chooseSpecie(void);
   int plantSpecie(void);
   void setSpecie(std::vector<int> sp, std::vector<float> spPunc);
@@ -29,16 +23,14 @@ void propriety::proprietyConstruct(int xInit, int yInit){
   LimL = xInit*propSizeRadix + propSizeRadix - 1;
   LimN = yInit*propSizeRadix;
   LimS = yInit*propSizeRadix + propSizeRadix - 1;
-  probInovation = gauss(rand64) - 0.5;
   personalPref = uniFLOAT(rand64);
-  weight[0] = gauss(rand64)-0.75;
-  weight[1] = gauss(rand64)-0.75;
-  weight[2] = gauss(rand64)-0.75;
-  weight[3] = 1 - weight[0] - weight[1] - weight[2];
 }
 
 void propriety::chooseSpecie(void){
   int PropSize = (1+LimL-LimO)*(1+LimL-LimO);
+  int j;
+  float n;
+  float acum, sum;
 
   if(speciesPunctuation.empty()){
     speciesChoosen.clear();
@@ -49,6 +41,37 @@ void propriety::chooseSpecie(void){
     return;
   }
 
+// Select only the variety with maximum punctuation
+
+  int idxMax = distance(speciesPunctuation.begin(), std::max_element(speciesPunctuation.begin(), speciesPunctuation.end()));
+  speciesChoosen = std::vector<int>(PropSize, availableSpecie[idxMax]);
+
+
+
+// Select nomalizing and choose based on that
+/*
+  speciesChoosen = std::vector<int>(PropSize, 0);
+  sum = std::accumulate(speciesPunctuation.begin(), speciesPunctuation.end(), 0.0);
+  acum = 0;
+  for(int i = 0; i < speciesPunctuation.size(); i++){
+    cout << speciesPunctuation[i] << ", ";
+    acum += speciesPunctuation[i]/sum;
+    speciesPunctuation[i] = acum;
+  }
+  cout << endl;
+  for(int i = 0; i < PropSize; i++){
+    n = uniFLOAT(rand64);
+    j = 0;
+    while(n > speciesPunctuation[j])
+      j++;
+    speciesChoosen[i] = availableSpecie[j];
+    //cout << j << ", ";
+  }
+  //cout << endl;
+*/
+
+/*
+// Select random variety and the probability of get planted is given by the punctuation
   uniIntne.param(std::uniform_int_distribution<long>::param_type(0, speciesPunctuation.size()-1));
   speciesChoosen = std::vector<int>(PropSize, 0);
 
@@ -59,15 +82,13 @@ void propriety::chooseSpecie(void){
     }
     speciesChoosen[i] = availableSpecie[plantIdx];
   }
+*/
+
 }
 
 void propriety::setSpecie(std::vector<int> sp, std::vector<float> spPunc){
   availableSpecie = sp;
   speciesPunctuation = spPunc;
-
-  int idxMax = distance(speciesPunctuation.begin(), std::max_element(speciesPunctuation.begin(), speciesPunctuation.end()));
-  maxSpecie = availableSpecie[idxMax];
-  maxSpeciePunc = speciesPunctuation[idxMax];
 }
 
 void propriety::addSpecie(int sp, float spPunc){
