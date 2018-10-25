86090064e9c8a8362aa6ccfa92e61fe9f0b21cc9
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2018-10-25 11:43:41 -0300

    Fix string parse and add axis label

--- src/plot.jl
+++ src/plot.jl
@@ -3,6 +3,7 @@ using DataFrames
 
 input_file = ARGS[1]
 mode = split(input_file, "_")[2]
+mode = split(mode, ".")[1]
 param = string(input_file[end-4])
 input_file = "test/" * input_file
 output_file = input_file[1:end-4] * ".svg"
@@ -10,12 +11,13 @@ output_file = input_file[1:end-4] * ".svg"
 df = readtable(input_file, separator = ';', skipstart=3)
 
 if mode == "standard"
-    p = plot(df, x=:meanDU, y=:nVar, Geom.line, Guide.ylabel("Numero de Variedades"), Guide.ylabel("Numero Medio de Variedades por UD"))
+    p = plot(df, x=:meanDU, y=:nVar, Geom.line, Guide.ylabel("Numero de Variedades"), Guide.xlabel("Numero Medio de Variedades por UD"))
     draw(SVG(output_file, 15cm, 10cm), p)
     println("Image $(output_file) successfully generated.")
     p2 =plot(layer(df, x=:time, y=:nVar, Geom.line, Theme(default_color=colorant"green")),
             layer(df, x=:time, y=:meanDU, Geom.line, Theme(default_color=colorant"red")),
-            Guide.manual_color_key("",["Número de Variedades","Número Médio de Variedades por UD"],["green", "red"]))
+            Guide.manual_color_key("",["Número de Variedades","Número Médio de Variedades por UD"],["green", "red"]),
+            Guide.ylabel(""), Guide.xlabel("Tempo"))
     output_file2 = input_file[1:end-4] * "2.svg"
     draw(SVG(output_file2, 20cm, 10cm), p2)
     println("Image $(output_file2) successfully generated.")
@@ -25,11 +27,14 @@ elseif mode == "varParam"
     println("Image $(output_file) successfully generated.")
 elseif mode == "varietyDistribution"
     if length(split(input_file, "_")) == 2
-        p = plot(df, x=:value, y=:varDist, Geom.bar, Coord.cartesian(xmin=0, xmax=20, ymin=0, ymax=1))
+        p = plot(df, x=:value, y=:varDist, Geom.bar, Coord.cartesian(xmin=0, xmax=20, ymin=0, ymax=1),
+                Guide.ylabel("Frequencia"), Guide.xlabel("Numero Medio de Variedades por UD"))
         draw(SVG(output_file, 15cm, 10cm), p)
         println("Image $(output_file) successfully generated.")
     elseif length(split(input_file, "_")) == 3
-        p = plot(df, x=:value, y=:varDist, color=:param, Geom.line, Scale.color_discrete(), Guide.colorkey(param), Coord.cartesian(xmin=0, xmax=20, ymin=0, ymax=1))
+        p = plot(df, x=:value, y=:varDist, color=:param, Geom.line, Scale.color_discrete(),
+                Guide.colorkey(param), Coord.cartesian(xmin=0, xmax=20, ymin=0, ymax=1),
+                Guide.ylabel("Frequencia"), Guide.xlabel("Numero Medio de Variedades por UD"))
         draw(SVG(output_file, 15cm, 10cm), p)
         println("Image $(output_file) successfully generated.")
     end
@@ -40,17 +45,20 @@ elseif mode == "histogramFitness"
     draw(SVG(output_file, 15cm, 10cm), p)
     println("Image $(output_file) successfully generated.")
 elseif mode == "histogramFitnessVar"
-    p = plot(df, x=:value, y=:fitness, color=:param, Geom.line, Scale.color_discrete(), Guide.colorkey(param), Scale.y_continuous(minvalue=0, maxvalue=0.2))
+    p = plot(df, x=:value, y=:fitness, color=:param, Geom.line, Scale.color_discrete(), Guide.colorkey(param),
+            Scale.y_continuous(minvalue=0, maxvalue=0.2), Guide.ylabel("Frequencia"), Guide.xlabel("Fitness"))
     draw(SVG(output_file, 15cm, 10cm), p)
     println("Image $(output_file) successfully generated.")
-    p2 = plot(df, x=:value, y=:appearence, color=:param, Geom.line, Scale.color_discrete(), Guide.colorkey(param), Scale.y_continuous(minvalue=0, maxvalue=0.1))
+    p2 = plot(df, x=:value, y=:appearence, color=:param, Geom.line, Scale.color_discrete(), Guide.colorkey(param),
+            Scale.y_continuous(minvalue=0, maxvalue=0.1), Guide.ylabel("Frequencia"), Guide.xlabel("Appearence"))
     output_file2 = input_file[1:end-4] * "2.svg"
     draw(SVG(output_file2, 15cm, 10cm), p2)
     println("Image $(output_file2) successfully generated.")
 elseif mode == "varParamFixedPoints"
     p=plot(layer(df, x=:param, y=:nVar, Geom.line, Theme(default_color=colorant"green")),
             layer(df, x=:param, y=:meanDU, Geom.line, Theme(default_color=colorant"red")),
-            Guide.manual_color_key("",["Número de Variedades","Número Médio de Variedades por UD"],["green", "red"]))
+            Guide.manual_color_key("",["Número de Variedades","Número Médio de Variedades por UD"],["green", "red"]),
+            Guide.ylabel(""), Guide.xlabel("Time"))
     draw(SVG(output_file, 20cm, 10cm), p)
     println("Image $(output_file) successfully generated.")
 else
