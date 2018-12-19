096372fda3c99f6db10edb9f28ab20c20f707da4
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2018-12-19 14:22:31 -0200

    Change default parameters value

--- src/Worker.hpp
+++ src/Worker.hpp
@@ -140,10 +140,10 @@ namespace worker{
     switch (param){
       case 'o':
         paramList = std::vector<float> {0.08, 0.085, 0.09, 0.095, 0.1, 0.125, 0.15, 0.175, 0.2, 0.25, 0.3};
-        break;
+       break;
       case 'i':
         paramList = std::vector<float> {0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1};
-        break;
+       break;
       case 'L':
         paramList = std::vector<float> {25, 36, 49, 64};
         break;
