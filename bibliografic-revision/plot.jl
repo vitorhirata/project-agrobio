435c50be85f36534ebf308134988548cf27b61b2
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2019-05-24 12:06:47 -0300

    Change plot to svg
    - Add function with general histogram for the three species.
    - Add plot padding so the image does not get cut.
    - Improve image legend
    - Upate all images

--- bibliografic-revision/plot.jl
+++ bibliografic-revision/plot.jl
@@ -3,13 +3,14 @@ using DataFrames
 using Colors
 using CSV
 using StatsBase
-
+import Cairo, Fontconfig
 
 function plotAll()
   df, df2, df3 = loadData()
   plotGeraneralData(df)
   plotAllHistogram(df)
   plotAllBoxplot(df2, df3)
+  plotGeneral(df)
 end
 
 function loadData()
@@ -27,29 +28,51 @@ function plotGeraneralData(df)
   dfClean = dropmissing(dfClean)
 
   dfMandioca = dfClean[dfClean[:Especie] .== "Mandioca", :]
-  plotComunidadexDU(dfMandioca, "mandioca", [23cm, 10cm])
+  plotComunidadexDU(dfMandioca, "mandioca", [23*1.3cm, 10*1.3cm])
 
   dfMilho = dfClean[dfClean[:Especie] .== "Milho", :]
-  plotComunidadexDU(dfMilho, "milho", [19cm, 10cm])
+  plotComunidadexDU(dfMilho, "milho", [19*1.3cm, 10*1.3cm])
 
   dfBatata = dfClean[dfClean[:Especie] .== "Batata", :]
-  plotComunidadexDU(dfBatata, "batata", [17cm, 10cm])
+  plotComunidadexDU(dfBatata, "batata", [17*1.3cm, 10*1.3cm])
 
-  img = SVG("plot/todos.svg", 17cm, 10cm)
+
+
+  img = PNG("plot/todos.png", 17*1.3cm, 10*1.3cm)
   p=plot(dfClean, x=:NmedioUD, y=:Ncomunidade, color=:Especie, Geom.point,
-    Guide.xlabel("N de Var Médio po UD"), Guide.ylabel("N de Var na Comunidade"),
-    Guide.colorkey(title="Especie"), Guide.title("Agrobiodiversidade"));
+    Guide.xlabel("N Var Médio por UD"), Guide.ylabel("N Var Comunidade"),
+    Guide.colorkey(title="Especie"), Guide.title("Agrobiodiversidade"),
+    Theme(point_size=2.5pt));
   draw(img, p)
 
 end
 
 function plotComunidadexDU(df, str, size)
-  img = SVG("plot/$(str).svg", size[1], size[2])
+  img = PNG("plot/$(str).png", size[1], size[2])
   p=plot(df, x=:NmedioUD, y=:Ncomunidade, color=:Fonte, Geom.point,
-    Guide.xlabel("N de Var Médio po UD"), Guide.ylabel("N de Var na Comunidade"),
-    Guide.colorkey(title="Fonte"), Guide.title("Agrobiodiversidade de $(str)"));
+    Guide.xlabel("N Var Médio por UD"), Guide.ylabel("N Var Comunidade"),
+    Guide.colorkey(title="Fonte"), Guide.title("Agrobiodiversidade de $(str)"),
+    Theme(plot_padding=[30pt, 60pt, 10pt, 10pt]));
   draw(img, p)
 end
+
+function plotGeneral(df)
+  dfUD = df[[:Especie, :NmedioUD]]
+  dfUD = dropmissing(dfUD)
+  img = PNG("plot/todosUD.png", 15*1.3cm, 10*1.3cm)
+  p=plot(dfUD, x=:Especie, y=:NmedioUD, Geom.boxplot,
+    Guide.xlabel("Espécie"), Guide.ylabel("N Var Médio por UD"));
+  draw(img, p)
+
+  dfComunidade = df[[:Especie, :Ncomunidade]]
+  dfComunidade = dropmissing(dfComunidade)
+  img = PNG("plot/todosComunidade.png", 15*1.3cm, 10*1.3cm)
+  p=plot(dfComunidade, x=:Especie, y=:Ncomunidade, Geom.boxplot,
+    Guide.xlabel("Espécie"), Guide.ylabel("N Var Comunidade"));
+  draw(img, p)
+
+end
+
 
 function plotAllHistogram(df)
   dfClean = df[[:Especie, :Ncomunidade, :NmedioUD]]
@@ -68,16 +91,17 @@ function plotHistogram(df, str, binNumber)
   array1=collect(skipmissing(df[:Ncomunidade]))
   array2=collect(skipmissing(df[:NmedioUD]))
 
-  img1 = SVG("plot/$(str)Comunidade.svg", 15cm, 10cm)
+  img1 = PNG("plot/$(str)Comunidade.png", 15*1.3cm, 10*1.3cm)
   p1=plot(x=array1, Geom.histogram(bincount=binNumber[1], density=true),
     Guide.xlabel("N Variedades na Comunidade"), Guide.ylabel("Frequencia"),
     Guide.title("Histograma Variedades na Comunidade: $(str). N = $(size(array1)[1])"))
   draw(img1, p1)
 
-  img2 = SVG("plot/$(str)UD.svg", 15cm, 10cm)
+  img2 = PNG("plot/$(str)UD.png", 15*1.3cm, 10*1.3cm)
   p2=plot(x=array2, Geom.histogram(bincount=binNumber[2], density=true),
     Guide.xlabel("N Variedades Medio na UD"), Guide.ylabel("Frequencia"),
-    Guide.title("Histograma Média Variedades por Unidade Domestica: $(str). N = $(size(array2)[1])"))
+    Guide.title("Histograma Média Variedades por Unidade Domestica: $(str). N = $(size(array2)[1])."),
+    Theme(plot_padding=[50pt, 90pt, 10pt, 10pt]))
   draw(img2, p2)
 
 end
@@ -93,25 +117,27 @@ function plotAllBoxplot(df, df2)
 
   dfBatata1 = df[df[:Especie] .== "Batata", :]
   dfBatata2 = df2[df2[:Especie] .== "Batata", :]
-  plotBoxplot(dfBatata1, "batata", dfBatata2, [12.5cm, 7.5cm])
+  plotBoxplot(dfBatata1, "batata", dfBatata2, [12.5*1.3cm, 7.5*1.3cm])
 end
 
-function plotBoxplot(df1, str, df2=nothing, size=[20cm,14cm])
-  outputFile = "plot/$(str)_ud_var.svg"
-  img = SVG(outputFile, size[1], size[2])
+function plotBoxplot(df1, str, df2=nothing, size=[20*1.3cm,14*1.3cm])
+  outputFile = "plot/$(str)_ud_var.png"
+  img = PNG(outputFile, size[1], size[2])
   sort!(df1, [order(:Data)])
   p = plot(df1, x=:Fonte, y=:NUD, Geom.boxplot(suppress_outliers=true),
     Guide.xlabel("Fonte"), Guide.ylabel("Porcentagem UD"),
-    Guide.title("Porcentagem de UDs cultivando cada variedade: $(str)"))
+    Guide.title("Porcentagem de UDs cultivando cada variedade: $(str)"),
+    Theme(plot_padding=[20pt, 60pt, 10pt, 10pt]))
   draw(img, p)
 
   if df2 != nothing
-    outputFile = "plot/$(str)_var_ud.svg"
-    img = SVG(outputFile, size[1], size[2])
+    outputFile = "plot/$(str)_var_ud.png"
+    img = PNG(outputFile, size[1], size[2])
     sort!(df2, [order(:Data)])
     p = plot(df2, x=:Fonte, y=:Nvariedades, Geom.boxplot(suppress_outliers=true),
       Guide.xlabel("Fonte"), Guide.ylabel("Número de Variedades"),
-      Guide.title("Número de Variedades por UD: $(str)"))
+      Guide.title("Número de Variedades por UD: $(str)"),
+      Theme(plot_padding=[20pt, 40pt, 10pt, 10pt]))
     draw(img, p)
   end
 end
