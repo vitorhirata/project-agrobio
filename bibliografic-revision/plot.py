89ddeb49627724d99227f282e0455a3ac0024f68
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2020-04-25 19:40:32 -0300

    Add case studies map plot for paper
    The map of case studies will contain only data for manioc, so the plot
    was updated removing maiz and potato case studies.

--- bibliografic-revision/plot.py
+++ bibliografic-revision/plot.py
@@ -13,9 +13,9 @@ world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
 fig, ax = plt.subplots(1)
 base = world.plot(ax=ax, color='white', edgecolor='black')
-gdf[gdf.Specie == 'Manioc'].plot(ax=base, color='red', markersize=7)
-gdf[gdf.Specie == 'Maiz'].plot(ax=base, color='blue', markersize=7)
-gdf[gdf.Specie == 'Potato'].plot(ax=base, color='green', markersize=7)
+gdf[gdf.Specie == 'Manioc'].plot(ax=base, color='black', markersize=6)
+#gdf[gdf.Specie == 'Maiz'].plot(ax=base, color='blue', markersize=7)
+#gdf[gdf.Specie == 'Potato'].plot(ax=base, color='green', markersize=7)
 _ = ax.axis('off')
 plt.show()
 
